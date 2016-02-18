0524ef123155a94839919b87c10aa9e44bc236b2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-02-19 00:49:32 +1300

    Sort documentation.

--- lib/http/accept/sort.rb
+++ lib/http/accept/sort.rb
@@ -21,6 +21,7 @@
 module HTTP
 	module Accept
 		module Sort
+			# This sorts items with higher priority first, and keeps items with the same priority in the same relative order.
 			def self.by_quality_factor(items)
 				# We do this to get a stable sort:
 				items.sort_by.with_index{|object, index| [-object.quality_factor, index]}
