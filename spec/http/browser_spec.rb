59f30fbca8f0379b197b9e2e0c665501237d8999
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-02-18 22:48:07 +1300

    Test against common web browser accept headers.

--- spec/http/browser_spec.rb
+++ spec/http/browser_spec.rb
@@ -19,6 +19,7 @@
 # THE SOFTWARE.
 
 require 'http/accept/media_types'
+require 'http/accept/content_type'
 
 RSpec.shared_examples "wildcard media range" do |env|
 	let(:wildcard_media_ranges) {[HTTP::Accept::MediaTypes::WILDCARD_MEDIA_RANGE]}
@@ -43,4 +44,69 @@ RSpec.describe HTTP::Accept::MediaTypes do
 		
 		expect(media_types[0]).to be === text_plain_media_range
 	end
+end
+
+RSpec.shared_context "server content types" do
+	let(:json_content_type) {HTTP::Accept::ContentType.new('application/json')}
+	let(:html_content_type) {HTTP::Accept::ContentType.new('text/html')}
+	let(:wildcard_media_range) {HTTP::Accept::MediaTypes::MediaRange.new('*/*')}
+	
+	let(:map) {HTTP::Accept::MediaTypes::Map.new}
+	let(:media_types) {HTTP::Accept::MediaTypes.parse(accept_header)}
+end
+
+RSpec.shared_examples "web browser" do
+	include_context "server content types"
+	
+	it "should match text/html" do
+		map << html_content_type
+		map << json_content_type
+		
+		object, media_range = map.for(media_types)
+		
+		expect(object).to be == html_content_type
+	end
+end
+
+RSpec.describe "Firefox Accept: headers" do
+	let(:accept_header) {"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+	it_behaves_like "web browser"
+end
+
+RSpec.describe "WebKit Accept: headers" do
+	let(:accept_header) {"application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5"}
+	it_behaves_like "web browser"
+end
+
+RSpec.describe "Safari 5 Accept: headers" do
+	let(:accept_header) {"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+	it_behaves_like "web browser"
+end
+
+RSpec.describe "Internet Explorer 8 Accept: headers" do
+	# http://stackoverflow.com/questions/1670329/ie-accept-headers-changing-why
+	let(:accept_header) {"image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*"}
+	it_behaves_like "web browser"
+end
+
+RSpec.describe "Opera Accept: headers" do
+	let(:accept_header) {"text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1"}
+	it_behaves_like "web browser"
+end
+
+RSpec.shared_examples "json api" do
+	include_context "server content types"
+	
+	it "should match application/json" do
+		map << json_content_type
+		
+		object, media_range = map.for(media_types)
+		
+		expect(object).to be == json_content_type
+	end
+end
+
+RSpec.describe "XMLHttpRequest Accept: headers" do
+	let(:accept_header) {"application/json"}
+	it_behaves_like "json api"
 end
